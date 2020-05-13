1ed605f8be1283a90ebff03b204e0915fbc8e0fe
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-13 15:30:19 -0300

    Column and row

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/material.dart';
 void main(){
   runApp( MaterialApp(
     title: 'Frases do dia!!',
-    home: Container(
-      color: Colors.white,
-    ),
+   home: Column(children: <Widget>[
+     Text("Texto 1"),
+     Text("Texto 2")
+   ],),
+   // home: Container(color: Colors.white,),
   ));
 }
