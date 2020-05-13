599ce51ba7bdbe88894df1e9cdbbcfde5c99ba4e
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-13 16:17:30 -0300

    formatação de texto

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,12 +1,29 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp( MaterialApp(
     title: 'Frases do dia!!',
-   home: Column(children: <Widget>[
-     Text("Texto 1"),
-     Text("Texto 2")
-   ],),
-   // home: Container(color: Colors.white,),
+    home: Container(
+      color: Colors.white,
+      child: Column(
+        children: <Widget>[
+          Text(
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ipsum diam.",
+          style: TextStyle(
+            fontSize: 35,
+            fontStyle: FontStyle.normal,
+            fontWeight: FontWeight.normal,
+            letterSpacing: 0,
+            wordSpacing: 0,
+            decoration: TextDecoration.overline,
+            decorationStyle: TextDecorationStyle.solid,
+            decorationColor: Colors.black12,
+            color: Colors.black87
+          ),
+          )
+        ],
+      ),
+    )
   ));
 }
