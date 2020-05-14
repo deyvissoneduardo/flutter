405d3d5f65debaf0a38d3cf7592df3f5c1ebdc0c
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-14 00:52:36 -0300

    espacamento

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -1,27 +1,41 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
-void main(){
-  runApp( MaterialApp(
-    title: 'Frases do dia!!',
-    home: Container(
-      color: Colors.white,
-      child: Column(
-        children: <Widget>[
-          FlatButton(
+void main() {
+  runApp(MaterialApp(
+      // debugShowCheckedModeBanner: false,
+      title: 'Frases do dia!!',
+      home: Container(
+        margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        // padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+        // color: Colors.white,
+        decoration:
+            BoxDecoration(border: Border.all(width: 3, color: Colors.grey)),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ipsum diam.',
+              textAlign: TextAlign.justify,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Text("Texto 2"),
+            )
+            /* FlatButton(
               onPressed: (){
                 print('btn pressionado');
               },
               child: Text(
-                'Clique aqui',
+                'Clique aqui' ,
                 style: TextStyle(
                   fontSize: 20,
                   color: Colors.blue,
                   decoration: TextDecoration.none
                 ),
               )
-          )
-         /* Text(
+          )*/
+            /* Text(
               "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ipsum diam.",
           style: TextStyle(
             fontSize: 35,
@@ -35,8 +49,7 @@ void main(){
             color: Colors.black87
           ),
           )*/
-        ],
-      ),
-    )
-  ));
+          ],
+        ),
+      )));
 }
