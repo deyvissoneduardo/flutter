4c3c4a53a106e54eb1cde893a829448385341bc8
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-15 23:30:36 -0300

    Interface

--- frases_do_dia/lib/main.dart
+++ frases_do_dia/lib/main.dart
@@ -2,129 +2,59 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 
-/*
-  Stateless = Widgets que nao podem ser alterados
-  Stateful = Widgets que podem ser alterados
- */
-void main() {
+void main(){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
-    home: HomeStateful(),
-    /*Container(
-         //margin: EdgeInsets.all(0),
-         margin: EdgeInsets.only(top: 40),
-        // color: Colors.white,
-        decoration:
-            BoxDecoration(border: Border.all(width: 3, color: Colors.grey)),
-        child: Image.asset(
-          "images/laptop.jpg",
-          fit: BoxFit.contain,
-        ),
-        /*Column(
-          children: <Widget>[
-            Text('T-1'),
-            Text('T-2'),
-            Text('T-3')
-            /*Text(
-              'Lorem ipsum',
-              textAlign: TextAlign.justify,
-            ),
-            Padding(
-              padding: EdgeInsets.all(20),
-              child: Text("Texto 2"),
-            )*/
-            /* FlatButton(
-              onPressed: (){
-                print('btn pressionado');
-              },
-              child: Text(
-                'Clique aqui' ,
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.blue,
-                  decoration: TextDecoration.none
-                ),
-              )
-          )*/
-            /* Text(
-              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nec ipsum diam.",
-          style: TextStyle(
-            fontSize: 35,
-            fontStyle: FontStyle.normal,
-            fontWeight: FontWeight.normal,
-            letterSpacing: 0,
-            wordSpacing: 0,
-            decoration: TextDecoration.overline,
-            decorationStyle: TextDecorationStyle.solid,
-            decorationColor: Colors.black12,
-            color: Colors.black87
-          ),
-          )*/
-          ],
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        ),*/
-      )*/
+    home: Home(),
   ));
 }
 
-class HomeStateful extends StatefulWidget {
+class Home extends StatefulWidget {
   @override
-  _HomeStatefulState createState() => _HomeStatefulState();
+  _HomeState createState() => _HomeState();
 }
 
-class _HomeStatefulState extends State<HomeStateful> {
-
-  var _texto = "My App desenvolvido em Flutter";
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    print('Build chamado');
     return Scaffold(
       appBar: AppBar(
-        title: Text( "My App" ),
-        backgroundColor: Colors.blue,
+        title: Text('Frase do Dia!!!'),
+        backgroundColor: Colors.lightBlueAccent,
       ),
       body: Container(
-        child: Column(
-          children: <Widget>[
-            RaisedButton(
-              onPressed: (){
-                setState(() {
-                  _texto = "Curso Flutter";
-                });
-              },
-              color: Colors.blueGrey,
-              child: Text("Clique aqui"),
-            ),
-            Text('Nome: $_texto')
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-
-    var _title = "My App";
-    return Scaffold(
-      appBar: AppBar(
-        title: Text( _title ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Padding(
         padding: EdgeInsets.all(16),
-        child: Text('Conteudo Pricinpal'),
-      ),
-      bottomNavigationBar: BottomAppBar(
-        color: Colors.lightBlueAccent,
-        child: Padding(
-          padding: EdgeInsets.all(16),
-          child: Row(
-            children: <Widget>[Text('Conteudo Botton')],
-          ),
+        width: double.infinity,
+        /*decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.black),
+        ),*/
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset("images/logo.png"),
+            Text(
+              "Clique aqui para gera a frase",
+              textAlign: TextAlign.justify,
+              style: TextStyle(
+                fontSize: 20,
+                fontStyle: FontStyle.italic,
+                color: Colors.black87
+              ),
+            ),
+            RaisedButton(
+                child: Text(
+                  "Nova Frase",
+                  style: TextStyle(
+                    fontSize: 25,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              color: Colors.blue,
+              onPressed: (){},
+            )
+          ],
         ),
       ),
     );
