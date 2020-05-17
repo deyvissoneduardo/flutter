d3cd3765767d96e3139099b1e9a25482f9541819
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-17 00:12:38 -0300

    logica do calculo

--- alcool_gasolina/lib/Home.dart
+++ alcool_gasolina/lib/Home.dart
@@ -10,6 +10,42 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   TextEditingController _controllerAlcool = TextEditingController();
   TextEditingController _controllerGasolina = TextEditingController();
+  String _textoResultado = "";
+
+  /****metodo que calcula qual opcao e melhor *******/
+  void _calcular(){
+    /**pega valor do  _controllerAlcool e transfoma em double*/
+    double precoAlcool = double.tryParse( _controllerAlcool.text );
+    /**pega valor do  _controllerGasolina transfoma em double*/
+    double precoGasolina = double.tryParse( _controllerGasolina.text );
+
+    if(precoAlcool == null || precoGasolina == null){
+      setState(() {
+        _textoResultado = "Número Invalido para o calculo, troque a (,) por (.) ";
+      });
+    }else {
+      /**
+       * Se o preço do álcool dividido pelo preço da gasolina
+       * for >= 0.7 e melhor abastecer com gasolina
+       * senão melhor alcoll
+       * */
+      if( ( precoAlcool / precoGasolina ) >= 0.7 ){
+        setState(() {
+          _textoResultado = "Melhor abastecer com gasolina";
+        });
+      }else {
+        setState(() {
+          _textoResultado = "Melhor abastecer com álcool";
+        });
+      }
+      _limpaCampo();
+    }
+  }
+
+  void _limpaCampo(){
+    _controllerAlcool.text = "";
+    _controllerGasolina.text = "";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,13 +98,13 @@ class _HomeState extends State<Home> {
                     "Calcular",
                     style: TextStyle(fontSize: 20),
                   ),
-                  onPressed: () {},
+                  onPressed: _calcular,
                 ),
               ),
               Padding(
                 padding: EdgeInsets.only(top: 20),
                 child: Text(
-                  "Resultado: ",
+                  _textoResultado,
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold),
                 ),
               )
