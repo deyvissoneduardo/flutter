077160bf8d8bdbf5735b91eb92c6dcd1d4dab7ac
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-30 23:23:18 -0300

    dados salvos e recuperados 'dados.json'

--- lista_de_tarefas/lib/Home.dart
+++ lista_de_tarefas/lib/Home.dart
@@ -1,4 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -6,75 +11,107 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listaTarefas = [];
+
+  Future<File> _getFile() async {
+    // caminho relativo onde esta sendo salvo
+    final diretorio = await getApplicationDocumentsDirectory();
+    return File("${diretorio.path}/dados.json");
+  }
+
+  _salvarArquivo() async {
+    var arquivo = await _getFile();
+
+    //Criar dados
+    Map<String, dynamic> tarefa = Map();
+    tarefa["titulo"] = "Ir ao mercado";
+    tarefa["realizada"] = false;
+    _listaTarefas.add(tarefa);
+
+    String dados = json.encode(_listaTarefas);
+    arquivo.writeAsString(dados);
+
+    //print("Caminho: " + diretorio.path );
+  }
+
+  _lerArquivo() async {
+    try {
+      // recupera arquivo salvo
+      final arquivo = await _getFile();
+      return arquivo.readAsString();
+    } catch (e) {
+      // caso error, recupera o script
+      print(e.toString());
+      return null;
+    }
+  }
+
+  @override
+  void initState() {
+    // faz alteracao anted de carregar o build
+    super.initState();
+    _lerArquivo().then((dados) {
+      setState(() {
+        _listaTarefas = json.decode(dados);
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    // iniciando lista de tarefas
-    List _listTask = ["toma café", "almoço", "jantar"];
-
+    //_salvarArquivo();
+    print("itens 1: " + _listaTarefas.toString());
 
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Lista de Tarefas"),
+      appBar: AppBar(
+        title: Text("Lista de tarefas"),
+        backgroundColor: Colors.purple,
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
           backgroundColor: Colors.purple,
-        ),
-        // location do btn
-        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
-        floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.add),
-            backgroundColor: Colors.purple,
-            onPressed: () {
-              // abre janela para adc nova task
-              showDialog(
-                  context: context,
-                builder: (context){
-                    return AlertDialog(
-                      title: Text("Adicionar Tarefa"),
-                      content: TextField(
-                        decoration: InputDecoration(
-                          labelText: "Digite nova tarefa"
-                        ),
-                        onChanged: (text){
-                          // captura texto digitado pelo usuario
-
-                        },
+          onPressed: () {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("Adicionar Tarefa"),
+                    content: TextField(
+                      decoration:
+                          InputDecoration(labelText: "Digite sua tarefa"),
+                      onChanged: (text) {},
+                    ),
+                    actions: <Widget>[
+                      FlatButton(
+                        child: Text("Cancelar"),
+                        onPressed: () => Navigator.pop(context),
                       ),
-                      /*
-                       * lista de widgets
-                       */
-                      actions: <Widget>[
-                        FlatButton(
-                            onPressed: () => Navigator.pop(context),
-                            child: Text("Cancelar")
-                        ),
-                        FlatButton(
-                            onPressed: (){
-                              // salvar task
-                              Navigator.pop(context);
-                            },
-                            child: Text("Salvar")
-                        )
-                      ],
-                    );
-                }
-              );
-            }
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-                child: ListView.builder(
-                  // conta lista e exibe na tela
-                    itemCount: _listTask.length,
-                    itemBuilder: (context, index) {
-                      return ListTile(
-                        title: Text(_listTask[index]),
-                      );
-                    }
-                )
-            )
-          ],
-        )
+                      FlatButton(
+                        child: Text("Salvar"),
+                        onPressed: () {
+                          //salvar
+
+                          Navigator.pop(context);
+                        },
+                      )
+                    ],
+                  );
+                });
+          }),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+                itemCount: _listaTarefas.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_listaTarefas[index]['titulo']),
+                  );
+                }),
+          )
+        ],
+      ),
     );
   }
 }
