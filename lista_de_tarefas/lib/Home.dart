d6cd9b75bc64cff08c4faf9e97012b2670a09116
deyvissoneduardo22 <deyvissoneduardo22@gmail.com>
2020-05-31 22:28:24 -0300

    desfazendo acao excluir com Snackbar

--- lista_de_tarefas/lib/Home.dart
+++ lista_de_tarefas/lib/Home.dart
@@ -15,6 +15,7 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   List _listaTarefas = [];
   TextEditingController _controllerTarefa = TextEditingController();
+  Map<String, dynamic> _ultimoRemovido = Map();
 
   Future<File> _getFile() async {
     // caminho relativo onde esta sendo salvo
@@ -67,29 +68,45 @@ class _HomeState extends State<Home> {
   }
 
   Widget criaItemLista(context, index) {
-    final position = _listaTarefas[index]['titulo'];
+   // final position = _listaTarefas[index]['titulo'];
 
     return Dismissible(
-        key: Key(position),
+        key: Key( DateTime.now().millisecondsSinceEpoch.toString() ),
         direction: DismissDirection.endToStart,
-        onDismissed: ( direction ){
+        onDismissed: (direction) {
+          //recupera ultimo item excluido
+          _ultimoRemovido = _listaTarefas[index];
           // remove item da lista
           _listaTarefas.removeAt(index);
           _salvarArquivo();
+          //snackbar
+          final snackbar = SnackBar(
+            duration: Duration(seconds: 3),
+            content: Text('Conteudo removido'),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  // action desfazer
+                  setState(() {
+                    _listaTarefas.insert(index, _ultimoRemovido);
+                  });
+                  _salvarArquivo();
+                }),
+          );
+          Scaffold.of(context).showSnackBar(snackbar);
         },
         background: Container(
-          color: Colors.red,
-          padding: EdgeInsets.all(16),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Icon(
-                Icons.delete,
-                color: Colors.white,
-              ),
-            ],
-          )
-        ),
+            color: Colors.red,
+            padding: EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Icon(
+                  Icons.delete,
+                  color: Colors.white,
+                ),
+              ],
+            )),
         child: CheckboxListTile(
           title: Text(_listaTarefas[index]['titulo']),
           value: _listaTarefas[index]['realizada'],
