0b086af869889cf44cea36bf05918a63a071c124
deyvisson eduardo <deyvissoneduardo22@gmail.com>
2020-08-01 18:47:32 -0300

    remove

--- consulta_cep/android/app/src/main/java/deyvissoneduardo/com/consultacep/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package deyvissoneduardo.com.consultacep;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
--- consulta_cep/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:consultacep/Home.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
-}
-
